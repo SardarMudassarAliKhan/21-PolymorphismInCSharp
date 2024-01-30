b9f897cbb7e059b0b710dc5a1ae7f6b53462f6b4
Mudassar Ali <mudassar.ali@vortx.com>
2024-01-30 22:17:18 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+class Shape
+{
+    public virtual void Draw()
+    {
+        Console.WriteLine("Drawing a shape");
+    }
+}
+
+class Circle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a circle");
+    }
+}
+
+class Square : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a square");
+    }
+}
+
+class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public double Add(double a, double b)
+    {
+        return a + b;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Method Overloading (Compile-Time Polymorphism)
+        Calculator calculator = new Calculator();
+        Console.WriteLine("Addition of integers: " + calculator.Add(5, 10));
+        Console.WriteLine("Addition of doubles: " + calculator.Add(3.5, 7.2));
+
+        // Method Overriding (Runtime Polymorphism)
+        Shape[] shapes = new Shape[2];
+        shapes[0] = new Circle();
+        shapes[1] = new Square();
+
+        foreach(Shape shape in shapes)
+        {
+            shape.Draw(); // Dynamic Polymorphism
+        }
+    }
+}
